41b7a792fed313a966acc53577923e1af25f6224
Samsano <svendsensamuel@gmail.com>
2023-11-24 21:06:34 +0100

    Updated .cpp and .h

--- FunnerMutators/FunnerMutators.h
+++ FunnerMutators/FunnerMutators.h
@@ -8,38 +8,11 @@
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
 
-class FunnerMutators: public BakkesMod::Plugin::BakkesModPlugin/*, public BakkesMod::Plugin::PluginSettingsWindow*//*, public BakkesMod::Plugin::PluginWindow*/
+class FunnerMutators: public BakkesMod::Plugin::BakkesModPlugin
 {
-
-	//std::shared_ptr<bool> enabled;
-
-	//Boilerplate
 	virtual void onLoad();
 	virtual void onUnload();
+	void boost();
 
-	// Inherited via PluginSettingsWindow
-	/*
-	void RenderSettings() override;
-	std::string GetPluginName() override;
-	void SetImGuiContext(uintptr_t ctx) override;
-	*/
-
-	// Inherited via PluginWindow
-	/*
-
-	bool isWindowOpen_ = false;
-	bool isMinimized_ = false;
-	std::string menuTitle_ = "FunnerMutators";
-
-	virtual void Render() override;
-	virtual std::string GetMenuName() override;
-	virtual std::string GetMenuTitle() override;
-	virtual void SetImGuiContext(uintptr_t ctx) override;
-	virtual bool ShouldBlockInput() override;
-	virtual bool IsActiveOverlay() override;
-	virtual void OnOpen() override;
-	virtual void OnClose() override;
-	
-	*/
 };
 
