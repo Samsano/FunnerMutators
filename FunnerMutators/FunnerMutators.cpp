81ea1bcd3d91ac9e09a68332739935c4f602bfd4
Samsano <svendsensamuel@gmail.com>
2023-11-25 14:33:40 +0100

    Updated some things

--- FunnerMutators/FunnerMutators.cpp
+++ FunnerMutators/FunnerMutators.cpp
@@ -6,18 +6,18 @@ BAKKESMOD_PLUGIN(FunnerMutators, "Adds funner mutators", plugin_version, PLUGINT
 
 std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
 
-void FunnerMutators::onLoad()
-{
+void FunnerMutators::onLoad() {
 	_globalCvarManager = cvarManager;
 	
 	LOG("FunnerMutators Initiated")
 }
 
-void FunnerMutators::onUnload()
-{
+void FunnerMutators::onUnload() {
+
 }
 
 void FunnerMutators::boost() {
 	if (!gameWrapper -> IsInFreeplay()) {return}
 	std::string gameMode = gameWrapper->GetGameEventAsServer().GetGameMode();
+	LOG("Currently in gammode: " + gameMode)
 }
--- FunnerMutators/FunnerMutators.h
+++ FunnerMutators/FunnerMutators.h
@@ -8,11 +8,9 @@
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
 
-class FunnerMutators: public BakkesMod::Plugin::BakkesModPlugin
-{
+class FunnerMutators: public BakkesMod::Plugin::BakkesModPlugin {
 	virtual void onLoad();
 	virtual void onUnload();
 	void boost();
-
 };
 
