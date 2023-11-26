2ef8fca9c6acdfc52d9885be541f26f3cbf1d158
Samsano <svendsensamuel@gmail.com>
2023-11-26 11:55:02 +0100

    Updated
    Made it check if User is in Freeplay

--- FunnerMutators/FunnerMutators.cpp
+++ FunnerMutators/FunnerMutators.cpp
@@ -9,7 +9,11 @@ std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
 void FunnerMutators::onLoad() {
 	_globalCvarManager = cvarManager;
 	
-	LOG("FunnerMutators Initiated")
+	LOG("FunnerMutators Initiated");
+
+	cvarManager->registerNotifier("CheckGamemode", [this](std::vector<std::string> args) {
+		boost();
+		}, "", PERMISSION_ALL);
 }
 
 void FunnerMutators::onUnload() {
@@ -17,7 +21,7 @@ void FunnerMutators::onUnload() {
 }
 
 void FunnerMutators::boost() {
-	if (!gameWrapper -> IsInFreeplay()) {return}
-	std::string gameMode = gameWrapper->GetGameEventAsServer().GetGameMode();
-	LOG("Currently in gammode: " + gameMode)
+	if (!gameWrapper->IsInFreeplay()) {return;}
+	bool gameMode = gameWrapper->IsInFreeplay();
+	LOG("Currently in gammode: " + gameMode);
 }
